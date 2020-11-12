575654c1f45a3076e2060c0a3aba3424cc3c1479
민경승2 [mks6853] <mks6853@nexon.co.kr>
2020-11-12 17:25:22 +0900

    transaction func

--- AdminServer/DBManager.js
+++ AdminServer/DBManager.js
@@ -1,19 +1,22 @@
 var mysql = require('mysql');
 var doQuery = require('./DoQuery')
 
-var globalDBConfig = {
-    host : '127.0.0.1',
-    port : '3306',
-    user : 'mks',
-    password : '1234',
-    database : 'doz3',
-    connectionLimit : 5
- };
+const globalDBConfig = require('../config.json').globalDB;
 
 class DBManager {
     constructor() {
         this.pool = [];
-        this.pool[0] = mysql.createPool(globalDBConfig);
+        this.pool[0] = mysql.createPool({
+            host: globalDBConfig.host,
+            port: globalDBConfig.port,
+            user: globalDBConfig.user,
+            password: globalDBConfig.password,
+            database: globalDBConfig.database,
+            charset: globalDBConfig.charset,
+            supportBigNumbers: true,
+            bigNumberStrings: true,
+            connectionLimit: globalDBConfig.connectionLimit
+        });
 
         // connection의 release함수 호출 후 이벤트를 설정
         this.pool[0].on('release', connection =>{
@@ -78,8 +81,40 @@ class DBManager {
 }
 dbMgr = new DBManager;
 
+function BeginTransaction(connection) {
+    return new Promise((resolved, rejected) => {
+        connection.beginTransaction((err) => {
+            if (err) {
+                rejected(err);
+                return;
+            }
 
+            resolved();
+        });
+    });
+}
 
+function Rollback(connection) {
+    connection.rollback(() => { });
+}
+
+function Commit(connection) {
+    return new Promise((resolved, rejected) => {
+        connection.commit((err) => {
+            if (err) {
+                Rollback(connection);
+                rejected(err);
+                return;
+            }
+            
+            resolved();
+        });
+
+    });
+}
 module.exports = {
     DBManager : dbMgr,
+    BeginTransaction : BeginTransaction,
+    Rollback : Rollback,
+    Commit : Commit
 };
